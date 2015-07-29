3bd3921dadac99603fd05c69e4fa30d6ebfcfecc
Hari Donthi <hari.donthi@gmail.com>
2015-07-28 22:29:08 -0400

    used varImpPlot to pick variables

--- competition.R
+++ competition.R
@@ -1,5 +1,6 @@
 # current: change weight of text and category variables
 # predict using 90% category, and 10% text variables
+# pick top x variables using importance(rf) / varImpPlot(rf)
 # had higher AUC than random forest on all vars with 30% split on train data
 # But, performed worse on Kaggle than plain random forest on all vars
 
@@ -27,23 +28,24 @@ DescriptionWordsTrain2$sold=NULL
 colnames(DescriptionWordsTrain2)[13]="condition2"
 eBayTrain3=cbind(eBayTrain2,DescriptionWordsTrain2)
 
-#Split eBayTrain3 into Traintrain & Traintest
+#Split Training data: eBayTrain3 into Traintrain & Traintest
 # set.seed(123)
 # spl = sample.split(eBayTrain3$sold,SplitRatio = 0.7)
 # eBayTraintrain = subset(eBayTrain3, spl==TRUE)
 # eBayTraintest = subset(eBayTrain3, spl==FALSE)
 # eBayTraintrain$description = NULL
 # eBayTraintest$description = NULL
+## end eval auc
 
 eBayTraintrain = eBayTrain3
 eBayTraintrain$description=NULL
 #create dataframe of category vars and randomForest
 trainDiscreteVars = eBayTraintrain[,1:10]
-rf1 = randomForest(sold~.-UniqueID,data=trainDiscreteVars)
+rf1 = randomForest(sold~biddable+startprice+productline+condition+color,data=trainDiscreteVars)
 
 #create dataframe of text vars and randomForest
 trainTextVars = eBayTraintrain[,-1:-8]
-rf2 = randomForest(sold~.-UniqueID,data=trainTextVars)
+rf2 = randomForest(sold~ X100+scratch+ipad+screen+work+condition2+use+condit+good+great+hous+veri+item+used+test,data=trainTextVars)
 
 #Compute test values & submit
 eBayTest2 = read.csv("eBayiPadTest.csv") #without stringsAsFactors=FALSE
@@ -57,8 +59,12 @@ eBayTest=cbind(eBayTest2,DescriptionWordsTest2)
 Predrf1 = predict(rf1,newdata=eBayTest, type="prob")[,2]
 Predrf2 = predict(rf2,newdata=eBayTest, type="prob")[,2]
 
+# validation start - used for testing when splitting training
+# Predrf1 = predict(rf1,newdata=eBayTraintest, type="prob")[,2]
+# Predrf2 = predict(rf2,newdata=eBayTraintest, type="prob")[,2]
 # ROCRrf = prediction(((.9*Predrf1)+(.1*Predrf2))/2,eBayTraintest$sold)
 # rf.auc = as.numeric(performance(ROCRrf,"auc")@y.values)
+# validation end
 
 MySubmissionrf = data.frame(UniqueID = eBayTest2$UniqueID, Probability1 = ((.9*Predrf1)+(.1*Predrf2)/2))
 write.csv(MySubmissionrf, "rf.csv", row.names=FALSE)
